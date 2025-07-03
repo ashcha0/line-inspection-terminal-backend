d1371b4c154e4e12c907864b5d6e14b6b65f137c
11isnotavailable <1706209184@qq.com>
2025-07-03 21:49:57 +0800

    全部增删改查

---
+++ src/main/java/org/example/agvinhand/AgvInhandApplication.java
@@ -0,0 +1,13 @@
+package org.example.agvinhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgvInhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgvInhandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/agvinhand/AgvInhandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.agvinhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgvInhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
